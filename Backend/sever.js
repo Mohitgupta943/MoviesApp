cfb0e05ae550c1ef5714f8a8de6bf05a0c190878
ug-prog01 <ug19981@gmail.com>
2023-08-27 15:15:16 +0530

    Wrapper functions for tmdb

--- Backend/sever.js
+++ Backend/sever.js
@@ -11,52 +11,52 @@ class Server {
 
 
 
- connectDB() {
-  this.con = this.mysql.createConnection({
-    host: "db4free.net",
-    database: "movies_app_db",
-    user: "tmdb_user",
-    password: "midb@1234"
-  });
-  this.con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-}
+  connectDB() {
+    this.con = this.mysql.createConnection({
+      host: "db4free.net",
+      database: "movies_app_db",
+      user: "tmdb_user",
+      password: "midb@1234"
+    });
+    this.con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  }
 
-callUser(res) {
+  callUser(res) {
 
-  var query = this.con.query('SELECT * from Users', function (err, result) {
-    if (err) {
-      console.log(err.toString());
-      res.status(500).send(err.toString());
-    } else {
-      console.log(result)
-      res.json(result);
-      
-    }
-  });
-}
+    var query = this.con.query('SELECT * from users', function (err, result) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send(err.toString());
+      } else {
+        console.log(result)
+        res.json(result);
+
+      }
+    });
+  }
 
 
-addUser(req,res){
-  console.log("server "+req+" "+req.username+" "+req.password)
-  let userName=req.username
-  let pass=req.password
-  let email=req.email
-  console.log("server "+req+" "+userName+" "+pass)
-  let query_st=`insert into users (username,email,pass) values ("${userName}","${email}","${pass}")`
-  var query = this.con.query(query_st, function (err, result) {
-    if (err) {
-      console.log(err.toString());
-      res.status(500).send(err.toString());
-    } else {
-      console.log(result)
-      res.json(result);
-      
-    }
-  });
-}
+  addUser(req, res) {
+    console.log("server " + req + " " + req.username + " " + req.password)
+    let userName = req.username
+    let pass = req.password
+    let email = req.email
+    console.log("server " + req + " " + userName + " " + pass)
+    let query_st = `insert into users (username,email,pass) values ("${userName}","${email}","${pass}")`
+    var query = this.con.query(query_st, function (err, result) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send(err.toString());
+      } else {
+        console.log(result)
+        res.json(result);
+
+      }
+    });
+  }
 }
 module.exports = Server
 
