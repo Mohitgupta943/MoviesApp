981e7bc7a7b7970e6a5bd31396ea6b89fa5a10e8
Mohit Gupta <mohitgupta943@gmail.com>
2023-08-27 21:05:56 +0530

    added login and signup api

--- Backend/functions/account.js
+++ Backend/functions/account.js
@@ -8,13 +8,26 @@ import Auth from '../src/auth.js'
 let api = express();
 api.use(express.json()) 
 const router = express.Router();
-
+let userService= new Auth()
 router.post('/signup', (req, res) =>{
     //let j=JSON.parse(JSON.stringify(req.body))
     let jsonReq=req.body
     console.log("geting data "+jsonReq+" "+" "+jsonReq.username)
-    let dbServer= new Auth()
-    dbServer.createUser(jsonReq,res)
+   
+    userService.createUser(jsonReq,res)
+    //userService.createToken(res)
+    //userService.createSessionId()
+
+}
+ );
+
+router.post('/login', (req, res) =>{
+    let jsonReq=req.body
+    console.log("geting data "+jsonReq+" "+" "+jsonReq.username)
+    userService.loginUser(jsonReq,res)
+    //userService.createToken(res)
+    //userService.createSessionId()
+
 }
  );
 
--- Backend/src/auth.js
+++ Backend/src/auth.js
@@ -1,59 +1,100 @@
-import {dbConnection,executeQuery} from '../connections.js'
-import Constants from '../constants.js'
-class Auth{
 
-    constructor(){
-        this.API_KEY='api_key=2e65e5f5c5ba081b6ec96ea651bafe73';
-        this.Request_API_URL="https://api.themoviedb.org/3/authentication/token/new"
+import Server from '../sever.js'; 
+
+
+class Auth {
+
+    constructor() {
+        this.dbserver= new Server()
+        this.dbserver.connectDB()
+        this.API_KEY = 'api_key=2e65e5f5c5ba081b6ec96ea651bafe73';
+        this.Request_API_URL = 'https://api.themoviedb.org/3/authentication/token/new?'
+        this.CREATE_SESSION_ID_URL='https://api.themoviedb.org/3/authentication/session/new?'
+        console.log("req url "+this.Request_API_URL+this.API_KEY)
+
+       
     }
 
-    createUser(req,res){
-        console.log("server "+req+" "+req.username+" "+req.password)
-        let userName=req.username
-        let pass=req.password
-        let email=req.email
-        console.log("server "+req+" "+userName+" "+pass)
-        let query_st=`insert into users (username,email,pass) values ("${userName}","${email}","${pass}")`
-        executeQuery(query_st).then((data)=>{
-            
-                fetch(this.Request_API_URL+this.API_KEY).then(res=>{
-                    console.log(res)
-        }).catch((error)=>{
-            console.log('error',error)
-            return error;
-        });
+    loginUser(req,res){
 
-        })
-        
-        
-        // var query = dbConnection.query(query_st, function (err, result) {
-        //   if (err) {
-        //     console.log(err.toString());
-        //     res.status(500).send(err.toString());
-        //   } else {
-        //     console.log(result)
-        //     res.json(result); 
-        //     console.log("user created")
-            
-        //   }
-        //});
-        let sessionId=this.getSessionId(email)
-      }
-    
-      getSessionId(){
-        console.log("creating session Id")
-      }
-      getAllUser(res) {
+        console.log("login "  + " " + req.username + " " + req.password)
+        let pass = req.password
+        let email = req.email
+        let query=`select * from users where email="${email}" and pass="${pass}"`
+        this.dbserver.con.query(query, function (err, result) {
+            if (err) {
+              console.log(err.toString());
+              res.status(500).send(err.toString());
+            } else {
+              console.log(result)
+              console.log("user created "+ result.insertId) 
+              res.send("success")
+            }
+          })    
+
+
+    }
+
+    createUser(req, res) {
+        console.log("server " + req + " " + req.username + " " + req.password)
+        let userName = req.username
+        let pass = req.password
+        let email = req.email
+        console.log("server " + req + " " + userName + " " + pass)
+        let query_st = `insert into users (username,email,pass) values ("${userName}","${email}","${pass}");`
+        this.addUser(query_st,res)
+    }
+
+    addUser(query,res) {
+        console.log("adding user to db")
+        this.dbserver.con.query(query, function (err, result) {
+              if (err) {
+                console.log(err.toString());
+                res.status(500).send(err.toString());
+              } else {
+                console.log(result)
+                this.userId= result.insertId
+                console.log("user created "+ result.insertId) 
+                res.send(result)
+              }
+            })          
+    }
+
+   
+    createSessionId(){
+        console.log("creating sessionId")
+        const options = {
+            method: 'POST',
+            headers: {
+              accept: 'application/json',
+              'content-type': 'application/json',
+              Authorization: 'Bearer 2e65e5f5c5ba081b6ec96ea651bafe73'
+            }
+          };
+          
+          fetch(this.CREATE_SESSION_ID_URL, options)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("session = "+data.session_id)
+                res.send(data)
+            }
+            )
+            .catch(err => console.error('error:' + err));
+
+    }
+
+   
+    getAllUser(res) {
         var query = this.con.query('SELECT * from Users', function (err, result) {
-          if (err) {
-            console.log(err.toString());
-            res.status(500).send(err.toString());
-          } else {
-            console.log(result)
-            res.json(result);
-            
-          }
+            if (err) {
+                console.log(err.toString());
+                res.status(500).send(err.toString());
+            } else {
+                console.log(result)
+                res.json(result);
+
+            }
         });
-      }
+    }
 }
-module.exports=Auth
+module.exports = Auth
